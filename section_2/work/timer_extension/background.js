d2f21ab9182d9e3929ecb78ce1f749c800052a71
Son Pham <sonpn1982@gmail.com>
2024-07-10 23:58:11 +0800

    Update timeLapsed

--- section_2/work/timer_extension/background.js
+++ section_2/work/timer_extension/background.js
@@ -1,34 +1,35 @@
 chrome.alarms.create({
-    periodInMinutes: 1 / 60,
-})
+  periodInMinutes: 1 / 60,
+});
 
 let timeLapsed;
 
-chrome.alarms.onAlarm.addListener((alarm) => {
-    chrome.storage.sync.get(["timeLapsed"], (res) => {
-        timeLapsed = res.timeLapsed ?? 10
+chrome.alarms.onAlarm.addListener(alarm => {
+  chrome.storage.sync.get(['notificationTime'], res => {
+    timeLapsed = res.notificationTime ?? 10;
+  });
+
+  chrome.storage.local.get(['timer'], res => {
+    const time = res.timer ?? 0;
+
+    chrome.storage.local.set({
+      timer: time + 1,
     });
 
-    chrome.storage.local.get(["timer"], (res) => {
-        const time = res.timer ?? 0
-        
-        chrome.storage.local.set({
-            timer: time + 1,
-        })
+    chrome.action.setBadgeText({
+      text: `${time + 1}`,
+    });
 
-        chrome.action.setBadgeText({
-            text: `${time + 1}`
-        })
+    if (time % timeLapsed === 0) {
+      chrome.notifications.create('notification-id', {
+        type: 'basic',
+        iconUrl: 'icon.png',
+        title: 'Chrome Timer Extension',
+        message: `${timeLapsed} seconds has passed!`,
+      });
+      console.log(`notificationTime is: {timeLapsed}`);
+    }
 
-        if (time % timeLapsed === 0) {
-            chrome.notifications.create("notification-id", {
-                type: "basic",
-                iconUrl: "icon.png",
-                title: "Chrome Timer Extension",
-                message: `${timeLapsed} seconds has passed!`,
-            });
-        }
-
-        console.log(`From background: ${time + 1}`);
-    })
-})
+    console.log(`From background: ${time + 1}`);
+  });
+});
--- section_2/work/timer_extension/options.js
+++ section_2/work/timer_extension/options.js
@@ -1,14 +1,15 @@
-const nameInput = document.getElementById("name-input");
-const saveBtn = document.getElementById("save-btn");
+const nameInput = document.getElementById('name-input');
+const timeInput = document.getElementById('time-input');
+const saveBtn = document.getElementById('save-btn');
 
-saveBtn.addEventListener("click", () => {
-    const name = nameInput.value;
-    const notificationTime = timeInput.value;
-    chrome.storage.sync.set({ name, notificationTime });
-    console.log(`Name: ${name}`);
-    console.log('Notification time: ' + notificationTime);
+saveBtn.addEventListener('click', () => {
+  const name = nameInput.value;
+  const notificationTime = timeInput.value;
+  chrome.storage.sync.set({ name, notificationTime });
+  console.log(`Name: ${name}`);
+  console.log('Notification time: ' + notificationTime);
 });
 
-chrome.storage.sync.get(["name"], (res) => {
-    nameInput.value = res.name ?? "???";
+chrome.storage.sync.get(['name'], res => {
+  nameInput.value = res.name ?? '???';
 });
