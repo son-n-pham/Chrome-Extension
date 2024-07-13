62d6172d998cc516c849d4b161de070bb1b2a7de
Son Pham <sonpn1982@gmail.com>
2024-07-13 17:34:02 +0800

    Complete section 1 of Timer

--- section_2/work/timer_extension/background.js
+++ section_2/work/timer_extension/background.js
@@ -10,30 +10,29 @@ chrome.alarms.onAlarm.addListener(alarm => {
   });
 
   chrome.storage.local.get(['isRunning'], res => {
-    if (!res.isRunning) {
-      return;
-    }
-  });
+    console.log(`From background: ${res.isRunning}`);
+    if (res.isRunning) {
+      chrome.storage.local.get(['timer'], res => {
+        const time = res.timer ?? 0;
 
-  chrome.storage.local.get(['timer'], res => {
-    const time = res.timer ?? 0;
+        chrome.storage.local.set({
+          timer: time + 1,
+        });
 
-    chrome.storage.local.set({
-      timer: time + 1,
-    });
+        chrome.action.setBadgeText({
+          text: `${time + 1}`,
+        });
 
-    chrome.action.setBadgeText({
-      text: `${time + 1}`,
-    });
-
-    if (time % notificationTime === 0) {
-      chrome.notifications.create('notification-id', {
-        type: 'basic',
-        iconUrl: 'icon.png',
-        title: 'Chrome Timer Extension',
-        message: `${notificationTime} seconds has passed!`,
+        if (time % notificationTime === 0) {
+          chrome.notifications.create('notification-id', {
+            type: 'basic',
+            iconUrl: 'icon.png',
+            title: 'Chrome Timer Extension',
+            message: `${notificationTime} seconds has passed!`,
+          });
+          console.log(`notificationTime is: ${notificationTime}`);
+        }
       });
-      console.log(`notificationTime is: ${notificationTime}`);
     }
 
     console.log(`From background: ${time + 1}`);
--- section_2/work/timer_extension/popup.js
+++ section_2/work/timer_extension/popup.js
@@ -1,50 +1,52 @@
-const timeElement = document.getElementById("time");
-const nameElement = document.getElementById("name");
-const timerElement = document.getElementById("timer");
-const startBtn = document.getElementById("start");
-const stopBtn = document.getElementById("stop");
-const resetBtn = document.getElementById("reset");
+const timeElement = document.getElementById('time');
+const nameElement = document.getElementById('name');
+const timerElement = document.getElementById('timer');
+const forDebugElement = document.getElementById('for-debug');
+const startBtn = document.getElementById('start');
+const stopBtn = document.getElementById('stop');
+const resetBtn = document.getElementById('reset');
 
-startBtn.addEventListener("click", () => {
-   chrome.storage.local.set({ isRunning: true });
+startBtn.addEventListener('click', () => {
+  chrome.storage.local.set({ isRunning: true });
 });
 
-stopBtn.addEventListener("click", () => {
-   chrome.storage.local.set({ isRunning: false });
+stopBtn.addEventListener('click', () => {
+  chrome.storage.local.set({ isRunning: false });
 });
 
-resetBtn.addEventListener("click", () => {
-   chrome.storage.local.set({ timer: 0, isRunning: false });
+resetBtn.addEventListener('click', () => {
+  chrome.storage.local.set({ timer: 0, isRunning: false });
 });
 
-chrome.storage.local.get(["isRunning"], res => {
-   if (res.isRunning) {
-      startBtn.style.display = "none";
-      stopBtn.style.display = "inline-block";
-   } else {
-      startBtn.style.display = "inline-block";
-      stopBtn.style.display = "none";
-   }
+chrome.storage.local.get(['timer'], ({ timer = 0 }) => {
+  timerElement.textContent = `The timer is at: ${timer} seconds`;
 });
 
-chrome.storage.local.get(["timer"], ({ timer = 0 }) => {
-   timerElement.textContent = `The timer is at: ${timer} seconds`;
+chrome.action.setBadgeText({ text: 'TIME' });
+
+chrome.storage.sync.get(['name'], ({ name = '???' }) => {
+  nameElement.textContent = `Your name is: ${name}`;
 });
 
-chrome.action.setBadgeText({ text: "TIME" });
+function popupContent() {
+  const currentTime = new Date().toLocaleTimeString();
+  timeElement.textContent = `The time is: ${currentTime}`;
 
-chrome.storage.sync.get(["name"], ({ name = "???" }) => {
-   nameElement.textContent = `Your name is: ${name}`;
-});
+  chrome.storage.local.get(['timer'], ({ timer = 0 }) => {
+    timerElement.textContent = `The timer is at: ${timer} seconds`;
+  });
 
-function updateTimeElements() {
-   const currentTime = new Date().toLocaleTimeString();
-   timeElement.textContent = `The time is: ${currentTime}`;
-
-   chrome.storage.local.get(["timer"], ({ timer = 0 }) => {
-      timerElement.textContent = `The timer is at: ${timer} seconds`;
-   });
+  chrome.storage.local.get(['isRunning'], ({ isRunning = false }) => {
+    forDebugElement.textContent = `isRunning: ${isRunning}`;
+    if (isRunning) {
+      startBtn.style.display = 'none';
+      stopBtn.style.display = 'inline-block';
+    } else {
+      startBtn.style.display = 'inline-block';
+      stopBtn.style.display = 'none';
+    }
+  });
 }
 
-updateTimeElements();
-setInterval(updateTimeElements, 1000);
+popupContent();
+setInterval(popupContent, 1000);
